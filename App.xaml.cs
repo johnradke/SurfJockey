e28112b9898b77629f076d842ee66501d49f8cf7
John Radke <jtradke@gmail.com>
2015-07-29 18:48:27 -0500

    futzing with escalation & registry

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,8 @@ using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,8 +16,20 @@ namespace SurfJockey
 {
     public partial class App : Application
     {
+        private const string _makeAppDefaultBrowserArg = "MAKEMEDEFAULT";
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            if (!IsAdministrator())
+            {
+                LaunchSelfToMakeDefaultBrowser();
+            }
+
+            if (IsAdministrator() && e.Args.Contains(_makeAppDefaultBrowserArg))
+            {
+                MakeDefaultBrowser();
+            }
+
             var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
             File.WriteAllText(Path.Combine(path, "test.txt"), "blah");
@@ -24,6 +38,29 @@ namespace SurfJockey
 
             var mainWindow = new MainWindow();
             mainWindow.Show();
+        }
+
+        private void MakeDefaultBrowser()
+        {
+            Shutdown();
+        }
+
+        private static void LaunchSelfToMakeDefaultBrowser()
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = Assembly.GetExecutingAssembly().CodeBase;
+            startInfo.Arguments = _makeAppDefaultBrowserArg;
+            startInfo.Verb = "runas";
+
+            Process.Start(startInfo);
+        }
+
+        private static bool IsAdministrator()
+        {
+            var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
